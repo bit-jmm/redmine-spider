95c30e205637253f0f45a7a8cde44d2d35b0dc26
jiangming <jqingming@gmail.com>
2018-11-25 20:48:56 +0800

    add wiki field

--- redmine/items.py
+++ redmine/items.py
@@ -3,20 +3,22 @@
 import scrapy
 
 class Plugin:
-    name = ''
-    img_src = ''
-    url = ''
-    author = ''
-    website = ''
-    repo = ''
-    register_date = ''
-    cur_version = ''
-    compatiable_version = ''
-    rating = ''
-    rating_count = ''
+    def __init__(self):
+        self.name = '-'
+        self.img_src = '-'
+        self.url = '-'
+        self.author = '-'
+        self.website = '-'
+        self.repo = '-'
+        self.register_date = '-'
+        self.cur_version = '-'
+        self.compatiable_version = '-'
+        self.rating = '-'
+        self.rating_count = '-'
+        self.wiki = '-'
 
 
     def __str__(self):
         return '|'.join([self.name, self.img_src, self.url, self.author, self.website,
                  self.repo, self.register_date, self.cur_version, self.compatiable_version,
-                 self.rating, self.rating_count])
+                 self.rating, self.rating_count, self.wiki])
--- redmine/spiders/plugin.py
+++ redmine/spiders/plugin.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+#from imp import reload
+#reload(sys)
+#sys.setdefaultencoding('utf8')
 import scrapy
 from redmine.items import Plugin
 
@@ -16,49 +17,71 @@ class PluginSpider(scrapy.Spider):
         p = Plugin()
         try:
             p.name = (' ').join(response.css('div#content > h2::text').extract_first().split(' ')[2:]).strip()
-        except:
-            p.name = '-'
+        except Exception, e:
+            print '\nname error:'
+            print e
         try:
             p.img_src = self.home_url + response.css('td > img.plugin_thumbnail::attr(src)').extract_first()
-        except:
-            p.img_src = '-'
+        except Exception, e:
+            print '\nno image:'
+            print e
         p.url = response.url
         try:
             p.author = response.css('a.user::text').extract_first().strip()
-        except:
-            p.author = '-'
+        except Exception, e:
+            print '\nauthor error:'
+            print e
         try:
             p.website = response.css('td > a::text')[1].extract().strip()
-        except:
-            p.website = '-'
+        except Exception, e:
+            print '\nno website:'
+            print e
         try:
             p.repo = response.css('td > a::text')[2].extract().strip()
-        except:
-            p.repo = '-'
+        except Exception, e:
+            print '\nno repo site:'
+            print e
         try:
             p.register_date = response.css('tr > td::text')[2].extract().strip()
-        except:
-            p.register_date = '-'
+        except Exception, e:
+            print '\nregister date error:'
+            print e
         try:
             p.cur_version = response.css('tr > td::text')[3].extract().strip()
-        except:
-            p.cur_version = '-'
+        except Exception, e:
+            print '\ncurrent version error:'
+            print e
         try:
             p.compatiable_version = response.css('tr > td::text')[4].extract().strip()
-        except:
-            p.compatiable_version = '-'
+        except Exception, e:
+            print '\ncompatiable version error:'
+            print e
         try:
             p.rating = response.css('tr > td > span.rating-count > span::attr(title)').extract_first().strip()
             if not p.rating:
                 p.rating = '-'
-        except:
-            p.rating = '-'
+        except Exception, e:
+            print '\nrating error:'
+            print e
         try:
             p.rating_count = response.css('tr > td > span.rating-count > a::text').re('(\w+)')[0]
             if not p.rating_count:
                 p.rating_count = '-'
-        except:
-            p.rating_count = '-'
+        except Exception, e:
+            try:
+                p.rating_count = response.css('tr > td > span.rating-count::text').re('(\w+)')[0]
+                if not p.rating_count:
+                    p.rating_count = '-'
+            except Exception, e1:
+                print '\nno rating count'
+                print e1
+        try:
+            p.wiki = '.'.join([st.replace('\n','.') for st in response.css('div.wiki::text').extract() if len(st.strip()) > 0])
+            if not p.wiki:
+                p.wiki = '-'
+        except Exception, e:
+            print '\nwiki:'
+            print e
         self.f.write(str(p)+'\n')
 
     def parse(self, response):
