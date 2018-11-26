5a81847670ff6abc9849827f30302fab63e19b6b
jiangming <jqingming@gmail.com>
2018-11-26 17:11:47 +0800

    fix bug

--- redmine/spiders/plugin.py
+++ redmine/spiders/plugin.py
@@ -11,77 +11,69 @@ class PluginSpider(scrapy.Spider):
     start_urls = ['http://www.redmine.org/plugins?page=1/']
     home_url = 'http://www.redmine.org'
     plugins = []
-    f = open('plugins.txt', 'wb')
+    f = open('plugins.txt', 'w')
 
     def parse_plugin(self, response):
         p = Plugin()
         try:
             p.name = (' ').join(response.css('div#content > h2::text').extract_first().split(' ')[2:]).strip()
-        except Exception, e:
-            print '\nname error:'
-            print e
+        except:
+            print('\nname error:')
         try:
             p.img_src = self.home_url + response.css('td > img.plugin_thumbnail::attr(src)').extract_first()
-        except Exception, e:
-            print '\nno image:'
-            print e
+        except:
+            print('\nno image:')
         p.url = response.url
         try:
             p.author = response.css('a.user::text').extract_first().strip()
-        except Exception, e:
-            print '\nauthor error:'
-            print e
+        except:
+            print('\nauthor error:')
+        j = 0
         try:
             p.website = response.css('td > a::text')[1].extract().strip()
-        except Exception, e:
-            print '\nno website:'
-            print e
+        except:
+            j += 1
+            print('\nno website:')
         try:
-            p.repo = response.css('td > a::text')[2].extract().strip()
-        except Exception, e:
-            print '\nno repo site:'
-            print e
+            p.repo = response.css('td > a::text')[2-j].extract().strip()
+        except:
+            j += 1
+            print('\nno repo site:')
         try:
-            p.register_date = response.css('tr > td::text')[2].extract().strip()
-        except Exception, e:
-            print '\nregister date error:'
-            print e
+            p.register_date = response.css('tr > td::text')[j+2].extract().strip()
+        except:
+            print('\nregister date error:')
         try:
-            p.cur_version = response.css('tr > td::text')[3].extract().strip()
-        except Exception, e:
-            print '\ncurrent version error:'
-            print e
+            p.cur_version = response.css('tr > td::text')[j+3].extract().strip()
+        except:
+            print('\ncurrent version error:')
         try:
-            p.compatiable_version = response.css('tr > td::text')[4].extract().strip()
-        except Exception, e:
-            print '\ncompatiable version error:'
-            print e
+            p.compatiable_version = response.css('tr > td::text')[j+4].extract().strip()
+        except:
+            print('\ncompatiable version error:')
         try:
             p.rating = response.css('tr > td > span.rating-count > span::attr(title)').extract_first().strip()
             if not p.rating:
                 p.rating = '-'
-        except Exception, e:
-            print '\nrating error:'
-            print e
+        except:
+            print('\nrating error:')
         try:
             p.rating_count = response.css('tr > td > span.rating-count > a::text').re('(\w+)')[0]
             if not p.rating_count:
                 p.rating_count = '-'
-        except Exception, e:
+        except:
             try:
                 p.rating_count = response.css('tr > td > span.rating-count::text').re('(\w+)')[0]
                 if not p.rating_count:
                     p.rating_count = '-'
-            except Exception, e1:
-                print '\nno rating count'
-                print e1
+            except:
+                print('\nno rating count')
         try:
             p.wiki = '.'.join([st.replace('\n','.') for st in response.css('div.wiki::text').extract() if len(st.strip()) > 0])
             if not p.wiki:
                 p.wiki = '-'
-        except Exception, e:
-            print '\nwiki:'
-            print e
+        except:
+            print('\nwiki:')
         self.f.write(str(p)+'\n')
 
     def parse(self, response):
